352b3b70c5b229f6900c4f518dfcf87c57afbead
Szucs Krisztian <szucs.krisztian@gmail.com>
2017-05-10 16:34:34 +0200

    attempt to explicitly define libssh2 lib and path

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo $PREFIX
+
+export LIBSSH2_LIBRARIES=libssh2
+export LIBSSH2_LIBRARY_DIRS=$PREFIX/lib
+
 mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX \
          -DCMAKE_PREFIX_PATH=$PREFIX \
