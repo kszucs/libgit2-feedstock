49105a7f106e12ab8836551dc9a5a92dea997508
Krisztián Szűcs <szucs.krisztian@gmail.com>
2017-05-11 09:01:15 +0200

    e

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo $PREFIX
-
-export LIBSSH2_LIBRARIES=ssh2
-export LIBSSH2_LIBRARY_DIRS=$PREFIX/lib
-
 mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX \
          -DCMAKE_PREFIX_PATH=$PREFIX \
